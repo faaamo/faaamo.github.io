ebf74443be6dc6b2c1212fb546b46673bef9d8e5
lysongzZ_ <642048384@qq.com>
2016-03-18 23:15:05 +0800

    1.2

--- js/control.js
+++ js/control.js
@@ -15,23 +15,23 @@ function toNext(id)
     else if(id == 1)
         window.location.href = "hp-book.html";
     else if(id == 2)
-        window.location.href = "dot-cloth.html";
+        window.location.href = "emotion.html";
 	else if(id == 3)
-        window.location.href = "InThe1980.html";
+        window.location.href = "emotion2.html";
 	else if(id == 4)
         window.location.href = "hp-book2.html";
     else if(id == 5)
-        window.location.href = "drawing-product.html";
+        window.location.href = "xiami.html";
 	else if(id == 6)
-        window.location.href = "hp-book-zhoubian.html";
+        window.location.href = "snake.html";
     else if(id == 7)
-        window.location.href = "sh-made.html";
+        window.location.href = "drawing-product.html";
     else if(id == 8)
-        window.location.href = "dr-wall.html";
+        window.location.href = "InThe1980.html";
     else if(id == 9)
-        window.location.href = "cau-are.html";
+        window.location.href = "innovation.html";
     else if(id == 10)
-        window.location.href = "dnp.html";
+        window.location.href = "dot-cloth.html";
     else if(id == 11)
         window.location.href = "personal-ehxibition.html";
 }
